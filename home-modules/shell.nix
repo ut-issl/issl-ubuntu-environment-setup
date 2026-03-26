4dc92f8b6fabf82cf21eae702645ea95a66bc37b
Riki Nakamura <riki.nakamura.edu@gmail.com>
2026-03-26 14:34:37 +0900

    ci: normalize file keys in shell modules

--- home-modules/shell.nix
+++ home-modules/shell.nix
@@ -2,9 +2,11 @@ _:
 
 {
   home = {
-    file.".config/issl/shell/env.sh".source = ../assets/shell/env.sh;
-    file.".config/issl/shell/rc.sh".source = ../assets/shell/rc.sh;
-    file.".config/issl/bash/.bash_profile".source = ../assets/bash/.bash_profile;
-    file.".config/issl/bash/.bashrc".source = ../assets/bash/.bashrc;
+    file = {
+      ".config/issl/shell/env.sh".source = ../assets/shell/env.sh;
+      ".config/issl/shell/rc.sh".source = ../assets/shell/rc.sh;
+      ".config/issl/bash/.bash_profile".source = ../assets/bash/.bash_profile;
+      ".config/issl/bash/.bashrc".source = ../assets/bash/.bashrc;
+    };
   };
 }
--- home-modules/zsh.nix
+++ home-modules/zsh.nix
@@ -3,7 +3,9 @@
 {
   home = {
     packages = [ pkgs.zsh ];
-    file.".config/issl/zsh/.zprofile".source = ../assets/zsh/.zprofile;
-    file.".config/issl/zsh/.zshrc".source = ../assets/zsh/.zshrc;
+    file = {
+      ".config/issl/zsh/.zprofile".source = ../assets/zsh/.zprofile;
+      ".config/issl/zsh/.zshrc".source = ../assets/zsh/.zshrc;
+    };
   };
 }
