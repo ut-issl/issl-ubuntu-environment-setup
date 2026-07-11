f9233bfa93e4bac7a6f6a230d9fcdc608f4dc0f0
Riki Nakamura <103401287+conjikidow@users.noreply.github.com>
2026-07-11 11:27:27 +0900

    feat: install typst (#373)

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 common_dir="$(cd -- "${COMMON_DIR:-$(dirname -- "${BASH_SOURCE[0]}")/..}" && pwd)"
 export COMMON_DIR="${common_dir}"
 
-for name in nix shell git dev cpp node python rust vim; do
+for name in nix shell git dev cpp node python rust typst vim; do
   printf '\n=== tests/test-%s.sh ===\n' "${name}"
   "${common_dir}/tests/test-${name}.sh"
 done
