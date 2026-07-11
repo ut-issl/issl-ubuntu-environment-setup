95b50593de0d9a2748ea913265b83d1fa7496d1f
Riki Nakamura <103401287+conjikidow@users.noreply.github.com>
2026-07-11 10:41:45 +0900

    feat: install nodejs (#370)
    * feat: install nodejs
    
    * docs: document node package management

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 common_dir="$(cd -- "${COMMON_DIR:-$(dirname -- "${BASH_SOURCE[0]}")/..}" && pwd)"
 export COMMON_DIR="${common_dir}"
 
-for name in nix shell git dev cpp python rust vim; do
+for name in nix shell git dev cpp node python rust vim; do
   printf '\n=== tests/test-%s.sh ===\n' "${name}"
   "${common_dir}/tests/test-${name}.sh"
 done
