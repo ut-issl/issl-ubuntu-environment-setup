9c5bf3d3f6c79b62e47511830c08ef691229066c
Riki Nakamura <103401287+conjikidow@users.noreply.github.com>
2026-08-11 14:07:02 +0900

    fix(modules): require a module directory to provide default.nix (#492)
    Directory entries were imported unconditionally, so a tracked directory that
    holds something other than a module, such as files referenced by `home.file`,
    made the evaluation fail on a missing `default.nix`. Import a directory only
    when it actually provides one.

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -4,8 +4,12 @@
   xdg.enable = true;
 
   imports = lib.mapAttrsToList (name: _: ./common + "/${name}") (
-    lib.filterAttrs (name: type: type == "directory" || lib.hasSuffix ".nix" name) (
-      builtins.readDir ./common
-    )
+    lib.filterAttrs (
+      name: type:
+      if type == "directory" then
+        builtins.pathExists (./common + "/${name}/default.nix")
+      else
+        lib.hasSuffix ".nix" name
+    ) (builtins.readDir ./common)
   );
 }
