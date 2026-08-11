75b70d69992b0642ddb8edb6237fde28356607db
Riki Nakamura <riki.nakamura.edu@gmail.com>
2026-08-11 13:34:52 +0900

    fix(modules): import only Nix modules from the module directory
    `builtins.readDir` also lists entries that are not Nix expressions, and passing
    one of them to the module loader fails the whole evaluation. Restrict the
    discovered entries to directories and to files ending in `.nix`.

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -3,5 +3,9 @@
 {
   xdg.enable = true;
 
-  imports = lib.mapAttrsToList (name: _: ./common + "/${name}") (builtins.readDir ./common);
+  imports = lib.mapAttrsToList (name: _: ./common + "/${name}") (
+    lib.filterAttrs (name: type: type == "directory" || lib.hasSuffix ".nix" name) (
+      builtins.readDir ./common
+    )
+  );
 }
